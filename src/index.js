9fa18e9383e6a62e577710739ec18887d6dc1296
BenCasson <bcasson11@gmail.com>
2022-07-08 14:12:23 -0500

    Update components import path

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './index.sass';
-import text from '../src/sidebar/sidebar';
+import text from './components/sidebar';
 
 const pageContainer = document.getElementById('container');
 pageContainer.appendChild(text);
