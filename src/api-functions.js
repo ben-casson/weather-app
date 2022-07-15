c86d6cf64a4c2f29079ad2af7d1630d607853170
Ben Casson <bcasson11@gmail.com>
2022-07-15 11:46:31 -0500

    Create function to fetch weather data

--- src/api-functions.js
+++ src/api-functions.js
@@ -1,1 +1,13 @@
 import API_KEY from "./api-key";
+
+export async function getWeatherData(location) {
+    let weatherAPI = `https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${API_KEY}`;
+    try {
+        const response = await fetch(weatherAPI);
+        const weatherData = await response.json();
+        console.log(weatherData);
+    }
+    catch(error) {console.error(error)}
+}
+
+//https://api.openweathermap.org/data/2.5/weather?q=London&APPID=1111111111
