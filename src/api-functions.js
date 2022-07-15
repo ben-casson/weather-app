0b39d5e732e439043915f0bd9dff142edf61b8d1
Ben Casson <bcasson11@gmail.com>
2022-07-15 16:17:41 -0500

    Edit fetchWeatherData function parameter

--- src/api-functions.js
+++ src/api-functions.js
@@ -1,7 +1,7 @@
 import { API_KEY } from "./api-key.js";
 import { Weather } from "./weather-model";
 
-export async function fetchWeatherData(location, weatherObject) {
+export async function fetchWeatherData(location) {
     let weatherAPI = `https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${API_KEY}&units=imperial`;
     try {
         const response = await fetch(weatherAPI);
