3341aaa1db16e1ab00b7e38325aeb18b806772fb
Ben Casson <bcasson11@gmail.com>
2022-07-15 12:16:52 -0500

    Store data from api in WeatherData object

--- src/api-functions.js
+++ src/api-functions.js
@@ -1,13 +1,13 @@
 import { API_KEY } from "./api-key.js";
 
+let WeatherData;
+
 export async function getWeatherData(location) {
     let weatherAPI = `https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${API_KEY}&units=imperial`;
     try {
         const response = await fetch(weatherAPI);
-        const weatherData = await response.json();
-        console.log(weatherData);
+        WeatherData = await response.json();
+        console.log(WeatherData);
     }
     catch(error) {console.error(error)}
 }
-
-//https://api.openweathermap.org/data/2.5/weather?q=London&APPID=1111111111
