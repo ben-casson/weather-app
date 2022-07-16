6de9ee40f414a11616c4e6e66c6a7295e096f7d2
Ben Casson <bcasson11@gmail.com>
2022-07-16 14:43:13 -0500

    Import updateDisplay function

--- src/api-functions.js
+++ src/api-functions.js
@@ -1,5 +1,6 @@
 import { API_KEY } from "./api-key.js";
 import { Weather } from "./weather-model";
+import { updateDisplay } from "./views.js";
 
 export async function fetchWeatherData(location) {
     let weatherAPI = `https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${API_KEY}&units=imperial`;
@@ -8,6 +9,7 @@ export async function fetchWeatherData(location) {
         const data = await response.json();
         console.log(data);
         const weather = Weather(data);
+        updateDisplay(weather);
     }
     catch(error) {console.error(error)}
 }
