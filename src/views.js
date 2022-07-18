2c92c43ed6d6455f4b0f12e1cf0943e6717482b8
BenCasson <bcasson11@gmail.com>
2022-07-18 10:00:45 -0500

    Create single 'updateDisplay' function
    The previous functions designed to update various parts of the display didn't need to be separated from each other.
    This is because they aren't called anywhere else. Wrapping their calls in a separate 'wrapper' function was redundant.
    Putting them all in one function doesn't seem to harm code readability/structure.

--- src/views.js
+++ src/views.js
@@ -3,11 +3,6 @@ import { setTimeOfDay } from './utilities';
 
 //import api function to use in event listener
 //export updateDisplay to api-functions
-export function updateDisplay(weatherObject) {
-    updateMainDisplay(weatherObject);
-    //updateExtraDisplay(weatherObject)
-    changeBackgroundImage(weatherObject);
-}
 
 const location = document.getElementById('current-location');
 const currentTemperature = document.getElementById('current-temperature-number');
@@ -15,55 +10,53 @@ const currentWeatherStatus = document.getElementById('current-temperature-status
 const highTemperature = document.getElementById('high-temperature');
 const lowTemperature = document.getElementById('low-temperature');
 
-function updateMainDisplay(weatherObject) {
+export function updateDisplay(weatherObject) {
     location.innerText = weatherObject.location;
     currentTemperature.innerText = weatherObject.currentTemperature;
     currentWeatherStatus.innerText = weatherObject.currentWeatherStatus;
     highTemperature.innerText = weatherObject.highTemperature;
     lowTemperature.innerText = weatherObject.lowTemperature;
-}
 
-// function updateExtraDisplay(weatherObject) {
-    
-// }
+    const pageContainer = document.getElementById('root');
 
-const pageContainer = document.getElementById('root');
-
-function setBackgroundImageBasedOnTimeOfDay(weatherObject, weatherStatus) {
-    setTimeOfDay(weatherObject);
-    if (weatherObject.timeOfDay == 'daytime') {
-        pageContainer.style.backgroundImage = `url(../src/assets/day-${weatherStatus}.jpg)`;
+    function setBackgroundImage(weatherObject, weatherStatus) {
+        setTimeOfDay(weatherObject);
+        if (weatherObject.timeOfDay == 'daytime') {
+            pageContainer.style.backgroundImage = `url(../src/assets/day-${weatherStatus}.jpg)`;
+        }
+        else {
+            pageContainer.style.backgroundImage = `url(../src/assets/night-${weatherStatus}.jpg)`;
+        }
     }
-    else {
-        pageContainer.style.backgroundImage = `url(../src/assets/night-${weatherStatus}.jpg)`;
-    }
-}
 
-function changeBackgroundImage(weatherObject) {
-    let weatherCondition = weatherObject.currentWeatherStatus;
-    switch (weatherCondition.toLowerCase()) {
-        case 'clouds':
-            setBackgroundImageBasedOnTimeOfDay(weatherObject, 'clouds');
-            break;
-        case 'thunderstorm':
-            setBackgroundImageBasedOnTimeOfDay(weatherObject, 'thunderstorm');
-            break;
-        case 'snow':
-            setBackgroundImageBasedOnTimeOfDay(weatherObject, 'snow');
-            break;
-        case 'clear':
-            setBackgroundImageBasedOnTimeOfDay(weatherObject, 'clear');
-            break;
-        case 'drizzle':
-            setBackgroundImageBasedOnTimeOfDay(weatherObject, 'rain');
-            break;
-        case 'rain':
-            setBackgroundImageBasedOnTimeOfDay(weatherObject, 'rain');
-            break;
-        default:
-            setBackgroundImageBasedOnTimeOfDay(weatherObject, 'mist');
-            break;    
-    }
+    ((weatherObject) => {
+        let weatherCondition = (weatherObject.currentWeatherStatus).toLowerCase();
+        switch (weatherCondition) {
+            case 'clouds':
+                setBackgroundImage(weatherObject, 'clouds');
+                break;
+            case 'thunderstorm':
+                setBackgroundImage(weatherObject, 'thunderstorm');
+                break;
+            case 'snow':
+                setBackgroundImage(weatherObject, 'snow');
+                break;
+            case 'clear':
+                setBackgroundImage(weatherObject, 'clear');
+                break;
+            case 'drizzle':
+                setBackgroundImage(weatherObject, 'rain');
+                break;
+            case 'rain':
+                setBackgroundImage(weatherObject, 'rain');
+                break;
+            //several rare conditions with no relevant image
+            //many of them are similar to 'mist'
+            default:
+                setBackgroundImage(weatherObject, 'mist');
+                break;    
+        }
+    })(weatherObject);
 }
 
 const userInput = document.getElementById('userInput');
