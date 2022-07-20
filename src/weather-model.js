085fda5fc3bd5279243a538a2bd09daf2321eb3d
BenCasson <bcasson11@gmail.com>
2022-07-20 11:30:47 -0500

    Replace nullish coalescing with ternary

--- src/weather-model.js
+++ src/weather-model.js
@@ -19,12 +19,12 @@ export const Weather = (data) => {
     let sunsetEpoch = parseInt(data.sys.sunset);
     let sunrise = convertEpochToNormalTime(parseInt(data.sys.sunrise));
     let sunset = convertEpochToNormalTime(parseInt(data.sys.sunset) - 43200);
-    let precipitation = data.rain['1h'] ?? '0';
+    let precipitation = data.rain != undefined ? data.rain['1h'] : '0';
     let feelsLikeTemperature = Math.round(data.main.feels_like);
     let humidity = Math.round(data.main.humidity) + '%';
     let wind = Math.round(data.wind.speed);
     let pressure = data.main.pressure;
-    let visibility = data.main.visibility;
+    let visibility = data.visibility;
 
     return { timeOfDay, setTimeOfDay, location, currentTemperature, currentWeatherStatus, 
              highTemperature, lowTemperature, sunriseEpoch, sunsetEpoch, sunrise, sunset, 
