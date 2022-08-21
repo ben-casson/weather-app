40d1614076304aff2a41e140ec29773b7b76b002
Ben Casson <bcasson11@gmail.com>
2022-08-21 18:22:57 -0500

    Stop clearing text input after weather search

--- src/app.js
+++ src/app.js
@@ -1,20 +1,20 @@
 import { fetchWeatherData } from "./api-functions";
 
-const userInput = document.getElementById('user-input');
+export const userInput = document.getElementById('user-input');
 const searchButton = document.getElementById('search-button');
 
 export function runApp() {
-    window.onload = () => {
-        fetchWeatherData('London');
-    }
+    // window.onload = () => {
+    //     fetchWeatherData('London');
+    // }
     document.addEventListener('keyup', (e) => {
         if (userInput.value.trim() != '' && e.key === 'Enter') {
             fetchWeatherData(userInput.value);
-            userInput.value = '';
+            // userInput.value = '';
         }
     });
     searchButton.addEventListener('click', () => {
         if (userInput.value.trim() != '') fetchWeatherData(userInput.value);
-        userInput.value = '';
+        // userInput.value = '';
     });
 }
