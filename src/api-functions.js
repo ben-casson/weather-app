37f03c17a765eebcae86a50b099d87219a463c33
Ben Casson <bcasson11@gmail.com>
2022-08-21 18:22:14 -0500

    Fetch data based on which units button is selected

--- src/api-functions.js
+++ src/api-functions.js
@@ -1,9 +1,13 @@
 import { API_KEY } from "./api-key.js";
 import { Weather } from "./weather-model";
 import { updateDisplay } from "./views.js";
+import { fahrenheitButton } from "./views.js";
 
 export async function fetchWeatherData(location) {
-    let weatherAPI = `https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${API_KEY}&units=imperial`;
+    let currentUnit = fahrenheitButton.classList.contains('selected') ? 'imperial' : 'metric';
+    
+    let weatherAPI = `https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${API_KEY}&units=${currentUnit}`;
+    
     try {
         const response = await fetch(weatherAPI);
         const data = await response.json();
