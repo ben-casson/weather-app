ea496999b1e710ec0c3a701527931e3e8af16f80
Ben Casson <bcasson11@gmail.com>
2022-08-21 18:39:25 -0500

    Enable search for window onload

--- src/app.js
+++ src/app.js
@@ -4,17 +4,15 @@ export const userInput = document.getElementById('user-input');
 const searchButton = document.getElementById('search-button');
 
 export function runApp() {
-    // window.onload = () => {
-    //     fetchWeatherData('London');
-    // }
+    window.onload = () => {
+        fetchWeatherData('London');
+    }
     document.addEventListener('keyup', (e) => {
         if (userInput.value.trim() != '' && e.key === 'Enter') {
             fetchWeatherData(userInput.value);
-            // userInput.value = '';
         }
     });
     searchButton.addEventListener('click', () => {
         if (userInput.value.trim() != '') fetchWeatherData(userInput.value);
-        // userInput.value = '';
     });
 }
